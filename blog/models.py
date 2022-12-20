9bc9e2aa2262e9bfab51a2d17bfb892dada6cca1
mahdighadiriii <111646898+mahdighadiriii@users.noreply.github.com>
2022-12-20 12:40:46 -0800

    created models and databases
    created database.
    created models for blog and website.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #author 
+    #image
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #tag
+    #category
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    publishead_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    udated_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return '{} - {}' .format(self.id ,self.title )
