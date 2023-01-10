7b33d0d845f3dc305091fe85c17695a959b73e28
mahdighadiriii <111646898+mahdighadiriii@users.noreply.github.com>
2023-01-10 20:22:59 +0330

    Filter posts by author
    Making URL and view and Html Tag

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,15 @@ from django.http import request
 from blog.models import Post
 # Create your views here.
 
-def blog_view(request, cat_name=None):
+def blog_view(request, cat_name=None, author_username=None):
     posts = Post.objects.filter(status = 1)
+    
     if cat_name:
         posts = posts.filter(category__name=cat_name)
+
+    if author_username:
+        posts = posts.filter(author__username = author_username)
+
     context = {'posts':posts}
     return render(request,'blog/blog-home.html', context)
 
