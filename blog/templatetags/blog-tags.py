6909cb54f4915d58fe30aced0ed17466f308b6cf
mahdighadiriii <111646898+mahdighadiriii@users.noreply.github.com>
2023-01-10 15:59:30 +0330

    created new url and view
    working on posts categories dynamic

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,6 +1,6 @@
 from django import template
 from blog.models import Post
-
+from blog.models import Category
 register = template.Library()
 
 @register.simple_tag(name='totalposts')
@@ -19,8 +19,17 @@ def snippet(value,arg=20):
     return value[:arg] + '...'
 
 
-@register.inclusion_tag('blog/popular-posts.html')
+@register.inclusion_tag('blog/blog-popularposts.html')
 def latestposts():
-    posts = Post.objects.filter(status=1).order_by('publishead_date')[:1]
+    posts = Post.objects.filter(status=1).order_by('publishead_date')[:3]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+        return {'categories':cat_dict}
 
